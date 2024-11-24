ab48d94aea1e0653c94a2f91df8cc2f365f92bce
doyourbest <creative555.dev@gmail.com>
2024-11-23 22:37:38 -0600

    [update] update main script
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
-from flask import request
 from twilio.jwt.access_token import AccessToken
 from twilio.jwt.access_token.grants import VoiceGrant
 from twilio.twiml.voice_response import VoiceResponse, Dial
@@ -21,6 +20,7 @@ app = Flask(__name__)
 CORS(app, resources={
     r"/*": {
         "origins": [
+            "http://localhost:3000",
             "https://regular-tahr-artistic.ngrok-free.app",
             "https://central-lioness-hopefully.ngrok-free.app"
         ],
@@ -32,59 +32,38 @@ CORS(app, resources={
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template(
-        'home.html',
-        title="In-browser calls with Twilio",
-    )
+    return render_template('home.html', title="In-browser calls with Twilio")
 
 @app.route('/token', methods=['GET'])
 def get_token():
     identity = request.args.get('identity', 'user')
 
-    # Create access token with credentials
-    access_token = AccessToken(
-        account_sid,
-        api_key,
-        api_key_secret,
-        identity=identity
-    )
-
-    # Create a Voice grant and add it to the token
-    voice_grant = VoiceGrant(
-        outgoing_application_sid=twiml_app_sid,
-        incoming_allow=True
-    )
+    access_token = AccessToken(account_sid, api_key, api_key_secret, identity=identity)
+    voice_grant = VoiceGrant(outgoing_application_sid=twiml_app_sid, incoming_allow=True)
     access_token.add_grant(voice_grant)
 
-    # Generate the token
-    return jsonify({
-        'token': access_token.to_jwt(),
-        'identity': identity
-    })
+    return jsonify({'token': access_token.to_jwt(), 'identity': identity})
 
 @app.route('/handle_calls', methods=['POST'])
 def call():
-    p.pprint(request.form)
     response = VoiceResponse()
     caller = request.form['Caller']
 
-    print(request.form['From'] + ' --> ' + request.form['To'])
+    print(f"{request.form['From']} --> {request.form['To']}")
 
     if 'To' in request.form and request.form['To'] in twilio_numbers:
-        print('incoming call')
-        # Start recording the incoming call
-        response.say("This call is being recorded.")
-        response.record(action='/handle_recording', max_length=60)
+        print('Incoming call')
+        # response.say("This call is being recorded.")
+        # response.record(action='/handle_recording', max_length=60)
 
         dial = Dial(callerId=caller)
         dial.client(request.form['To'].replace('+', ''))
         response.append(dial)
     else:
-        print('outbound call')
+        print('Outbound call')
         caller = request.form['From'].replace('client:', '+')
-        # Start recording the outgoing call
-        response.say("This call is being recorded.")
-        response.record(action='/handle_outgoing_recording', max_length=60)
+        # response.say("This call is being recorded.")
+        # response.record(action='/handle_outgoing_recording', max_length=60)
 
         dial = Dial(callerId=caller)
         dial.number(request.form['To'])
@@ -96,7 +75,6 @@ def call():
 def handle_recording():
     recording_url = request.form['RecordingUrl']
     print(f"Incoming call recording available at: {recording_url}")
-
     # Save the recording URL or process it as needed
     return "Recording received", 200
 
@@ -104,7 +82,6 @@ def handle_recording():
 def handle_outgoing_recording():
     recording_url = request.form['RecordingUrl']
     print(f"Outgoing call recording available at: {recording_url}")
-
     # Save the recording URL or process it as needed
     return "Recording received", 200
 
